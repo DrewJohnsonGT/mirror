8fec5face6ea0efdf97c6a6c3839fe8c8b7dba82
Drew Johnson <drew.johnson@jupiterone.com>
2024-01-05 12:58:44 -0500

    Remove christmas compliments and countdown

--- src/hooks/useCompliments.ts
+++ src/hooks/useCompliments.ts
@@ -156,7 +156,7 @@ const getTimeOfDay = () => {
 
 const getTimeOfYear = () => {
   const month = new Date().getMonth();
-  if (month >= 11 || month <= 1) {
+  if (month >= 11 || month <= 12) {
     return TimeOfYear.Winter;
   }
   if (month >= 2 && month <= 4) {
