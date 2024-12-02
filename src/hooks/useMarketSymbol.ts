3b5986961f3c160d7b97f1583eaf45e5a5598597
Drew Johnson <drewjohnsongt@gmail.com>
2024-12-02 14:56:15 -0500

    Market price update function inside useEffect

--- src/hooks/useMarketSymbol.ts
+++ src/hooks/useMarketSymbol.ts
@@ -4,19 +4,18 @@ import { CRYPTO_REFRESH_RATE } from 'util/constants';
 export const useMarketSymbol = ({ symbol }: { symbol: string }) => {
   const [currentPrice, setCurrentPrice] = useState(0);
 
-  const updatePrice = () => {
-    fetch('/api/crypto?symbol=' + symbol)
-      .then(async (res) => await res.json())
-      .then((price) => {
-        setCurrentPrice(parseInt(price));
-      })
-      .catch((e) => {
-        console.log('ERROR FETCHING CRYPTO');
-        console.error(e);
-      });
-  };
-
   useEffect(() => {
+    const updatePrice = () => {
+      fetch('/api/crypto?symbol=' + symbol)
+        .then(async (res) => await res.json())
+        .then((price) => {
+          setCurrentPrice(parseInt(price));
+        })
+        .catch((e) => {
+          console.log('ERROR FETCHING CRYPTO');
+          console.error(e);
+        });
+    };
     updatePrice();
     const marketSymbolInterval = setInterval(() => {
       updatePrice();
